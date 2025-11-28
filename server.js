4ab905fe846d5eac0b64b9d12baed8bbf7724408
krisnsingh27 <krisnsingh@bestpeers.in>
2025-11-28 18:32:00 +0530

    Your descriptive commit message here

--- server.js
+++ server.js
@@ -6,15 +6,22 @@ const authMiddleware = require("./middleware/authMiddleware");
 const userRoutes = require("./routes/userRoutes");
 const connectionRoutes = require("./routes/connectionRoutes")
 const cookieParser = require("cookie-parser");
+const cors=require("cors")
 
 
 
 const app = express();
 
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 
 app.use("/api/auth", authRoutes);
